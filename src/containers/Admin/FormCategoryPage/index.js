39019a6023d63a92a877fc35ca00b79e396a443c
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-02 17:54:42 -0300

    changes were added

--- src/containers/Admin/FormCategoryPage/index.js
+++ src/containers/Admin/FormCategoryPage/index.js
@@ -2,15 +2,46 @@ import React, { useState } from 'react';
 import BannerSection from "../../../components/BannerSection";
 import FollowUs from "../../../components/FollowUs";
 import SimpleReactValidator from 'simple-react-validator';
+import api from '../../../utils/api';
+
 
 const FormCategoryPage = () => {
   const validator = new SimpleReactValidator();
   const [name, setName] = useState('');
   const [color, setColor] = useState('');
+  const [politics, setPolitics] = useState('');
+  const [sport, setSport] = useState('');
+  const [economy, setEconomy] = useState('');
+  const [police, setPolice] = useState('');
+  const [society, setSociety] = useState('');
+  const [national, setNational] = useState('');
+  const [provice, setProvice] = useState('');
+  const [world, setWorld] = useState('');
 
-
-  const submitHandler = () => {
-
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const data = {
+      name,
+      color, 
+      politics,
+      sport,
+      economy,
+      police,
+      society,
+      national,
+      provice, 
+      world,
+    };
+    try {
+      const response = await api.category.add(data,
+        { header: { 'Content-Type': 'application/json' } }
+        );
+        if (response) {
+          console.log(response.data)
+        }
+    } catch (error) {
+      console.log(error);
+    } 
   }
 
   return (
@@ -35,9 +66,49 @@ const FormCategoryPage = () => {
                           {validator.message('Titulo', name, 'required')}
                         </div>
                         <div className="col-lg-6">
-                          <input name="Date" value={color} onChange={e => setColor(e.target.value)}
+                          <input name="color" value={color} onChange={e => setColor(e.target.value)}
                             type="text"
                             placeholder="color" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="politics" value={politics} onChange={e => setPolitics(e.target.value)}
+                            type="text"
+                            placeholder="politics" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="sport" value={sport} onChange={e => setSport(e.target.value)}
+                            type="text"
+                            placeholder="sport" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="economy" value={economy} onChange={e => setEconomy(e.target.value)}
+                            type="text"
+                            placeholder="economy" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="police" value={police} onChange={e => setPolice(e.target.value)}
+                            type="text"
+                            placeholder="police" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="society" value={society} onChange={e => setSociety(e.target.value)}
+                            type="text"
+                            placeholder="society" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="national" value={national} onChange={e => setNational(e.target.value)}
+                            type="text"
+                            placeholder="national" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="province" value={province} onChange={e => setProvince(e.target.value)}
+                            type="text"
+                            placeholder="province" />
+                        </div>
+                        <div className="col-lg-6">
+                          <input name="world" value={world} onChange={e => setWorld(e.target.value)}
+                            type="text"
+                            placeholder="world" />
                         </div>
                         <div className="col-12">
                           <div className="space-20" />
