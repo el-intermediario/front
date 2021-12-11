9699e3baf5e15a01c3c73d6f0d7550e93fff952d
Matias Torres <matiastorres.dev@gmail.com>
2021-12-11 18:21:17 -0300

    Add modal video.

--- src/components/VideoGallery/index.js
+++ src/components/VideoGallery/index.js
@@ -1,16 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
-import FontAwesome from "../uiStyle/FontAwesome";
-import Swiper from 'react-id-swiper';
-import {mostViewSort} from "../../utils/commonFunctions";
+import ModalVideo from 'react-modal-video'
 import videoPlaceholder from '../../doc/img/bg/video_bg.jpg';
-
-// images
-import popularsm1 from '../../doc/img/popular/popularsm1.jpg';
-import popularsm2 from '../../doc/img/popular/popularsm2.jpg';
-import popularsm3 from '../../doc/img/popular/popularsm3.jpg';
-import popularsm4 from '../../doc/img/popular/popularsm4.jpg';
-import popularsm5 from '../../doc/img/popular/popularsm5.jpg';
 
 import './style.scss';
 import api from '../../utils/api';
@@ -18,6 +9,8 @@ import api from '../../utils/api';
 const VideoGallery = ({items}) => {
     const [swiper, setSwiper] = useState(null);
     const [videos, setVideos] = useState(items);
+    const [vModal, setvModal] = useState(false);
+    const [currentVideo, setCurrentVideo] = useState(null);
 
     useEffect(() => {
       if (items.length) {
@@ -32,6 +25,11 @@ const VideoGallery = ({items}) => {
       setVideos(newItems);
     }
 
+    const videoHandle = (video) => {
+      setCurrentVideo(video);
+      setvModal(true);
+    }
+
     return (
         <div className="popular_carousel_area mb30 md-mt-30">
             <h2 className="widget-title">Destacados</h2>
@@ -40,7 +38,7 @@ const VideoGallery = ({items}) => {
                   <div key={i} className="single_post type10 widgets_small mb15">
                       <div className="post_img">
                           <div className="img_wrap">
-                              <Link to="#">
+                              <Link to="#" onClick={() => videoHandle(item)}>
                                   <img 
                                     src={item.thumbnail}
                                     onError={() => onErrorImage(i)}
@@ -50,13 +48,28 @@ const VideoGallery = ({items}) => {
                           <span className="tranding tranding_border">{item.id}</span>
                       </div>
                       <div className="single_post_text">
-                          <h4><Link to="#">{item.title}</Link></h4>
+                          <h4 onClick={() => videoHandle(item)}>{item.title}</h4>
                           <div className="meta4"><Link to="/">{item.category}</Link>
                           </div>
                       </div>
                   </div>
               ))}
             </div>
+            {currentVideo ? currentVideo.type === 'custom' ? (
+              <ModalVideo 
+                  channel={currentVideo && currentVideo.type} 
+                  url={currentVideo && api.space + currentVideo.content} 
+                  isOpen={vModal}
+                  onClose={() => setvModal(false)} 
+                />
+              ) : currentVideo.type === 'youtube' ? (
+              <ModalVideo 
+                channel={currentVideo && currentVideo.type} 
+                isOpen={vModal}
+                videoId={currentVideo && currentVideo.content}
+                onClose={() => setvModal(false)} 
+              />
+            ) : null : null}
         </div>
     );
 };
