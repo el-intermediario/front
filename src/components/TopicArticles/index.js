f80165638912a6c936744583db6831e67e426dfe
Matias Torres <matiastorres.dev@gmail.com>
2022-03-18 00:13:40 -0300

    Update styles off topic blocks.

--- src/components/TopicArticles/index.js
+++ src/components/TopicArticles/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../utils/api';
-import LazyImage from '../LazyImage';
+import "./styles.scss";
 
 const TopicArticles = ({ dark, topic }) => {
   const [articles, setArticles] = useState([]);
@@ -17,26 +17,12 @@ const TopicArticles = ({ dark, topic }) => {
       );
 
       if (response.data) {
-        console.log(response.data);
         setArticles(response.data);
       }
     } catch (err) {
       console.log(err);
     }
   }
-  const item1 = {
-    title: '28 casos positivos',
-    slug: 'titulo-de-la-nota',
-    image: 'https://www.telam.com.ar/thumbs/bluesteel/advf/imagenes/2022/01/61d9e095c301e_900.jpg'
-  }
-
-  const item2 = {
-    title: 'Argentina clasificada',
-    slug: 'titulo-de-la-nota',
-    image: 'https://www.telam.com.ar/thumbs/bluesteel/advf/imagenes/2021/07/60e11ab8bd7cc_900.jpg'
-  }
-
-  const item = topic === 'covid' ? item1 : item2;
 
   const bg = {
     backgroundImage: `url("./images/blocks/${topic}.jpg")`,
@@ -45,6 +31,10 @@ const TopicArticles = ({ dark, topic }) => {
     marginBottom: '20px',
     minHeight: '300px'
   };
+
+  if (articles.length === 0) {
+    return <></>
+  }
 
   return (
     <div className={`row row-topic topic-${topic}`} style={bg}>
@@ -59,23 +49,19 @@ const TopicArticles = ({ dark, topic }) => {
         <div className="row">
           {articles.map(item => (
             <div className="col-3">
-              <div className="single_post post_type6 post_type7">
-                <div className="post_img gradient1">
-                  <Link to={`/articulo/${item.slug}`}>
-                    <img src={`${api.space}${item.image}`}  alt={item.title} />
-                    {/* <img src={item.children[0].data.image} alt="thumb" /> */}
-                  </Link>
-                </div>
-                <div className="single_post_text">
-                  <div className="field-title">
-                    <Link to={`/articulo/${item.slug}`}>{item.title}</Link>
+              <div className="single_post" style={{backgroundImage: `url(${api.space}${item.image})`}}>
+                <Link to={`/articulo/${item.slug}`}>
+                  <div className="single_post_text">
+                    <div className="field-title">
+                      <Link to={`/articulo/${item.slug}`}>{item.title}</Link>
+                    </div>
                   </div>
-                </div>
+                </Link>
               </div>
             </div>
           ))}
-          <div className="space-30" />
         </div>
+        <div className="space-30" />
       </div>    
     </div>
   );
